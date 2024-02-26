b21b93cafa2ba5893f4100f61032154d8aae18f5
José Viejo HUerta <Jviejo@users.noreply.github.com>
2024-02-26 14:03:37 +0100

    cambios

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const axios = require('axios')
 require('dotenv').config()
 
-// console.log(process.env.PRIVATE.split("\\n").join("\n"))
+console.log(process.env.PORT)
+PORT = process.env.PORT 
 
 const key = process.env.PRIVATE.split("\\n").join("\n")
 https.createServer({
@@ -19,14 +21,29 @@ https.createServer({
 
     requestCert: true,
     rejectUnauthorized: false,
-}, app).listen(process.env.PORT | 8433, function () {
-    console.log(`Servidor https correindo en el puerto ${process.env.PORT | 8443}`);
+}, app).listen(PORT, function () {
+    console.log(`Servidor https correindo en el puerto ${PORT}`);
 });
 
 
+
 app.get('/', async function (req, res) {
-    const cert = Buffer.from(JSON.stringify(req.socket.getPeerCertificate())).toString("base64Url")
-    res.redirect(`${req.query.url}/cert=${cert}`)
+    try {
+        const cert = Buffer.from(JSON.stringify(req.socket.getPeerCertificate())).toString("base64Url")
+        const data = await axios.post(`${req.query.url}/api/fun/${req.query.idOrga}/cert`,
+            {
+                cert: cert
+            }
+        )
+        // console.log(data.data.token)
+        res.redirect(`${req.query.url}/?token=${data.data.token}`)
+    }
+    catch (err) {
+        console.log(err)
+        console.log(`error ${req.query.url}/api/fun/${req.query.idOrga}/cert`)
+        res.redirect(`${req.query.url}/api/fun/${req.query.idOrga}/cert?error=se ha producido un error`)
+    }
 })
+
 
 module.exports = app;
